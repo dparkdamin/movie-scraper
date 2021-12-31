0652bfa9ae7fe14c6af695e905f5cd2a2b4cbd73
Damin Park <dparkdamin@gmail.com>
2021-12-30 22:25:59 -0600

    movie class

---
+++ movie.py
@@ -0,0 +1,8 @@
+class Movie:
+    def __init__(self, rating: str, duration: str, description: str, year: str, image: str):
+        self.rating = rating
+        self.duration = duration
+        self.description = description
+        self.year = year
+        self.image = image
+
